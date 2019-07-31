422ba3a54f0433d15f8b1a07f7080ee092f013df
Sumit Chakraborty <Sumit.Chakraborty@iiitb.org>
2019-07-31 11:21:27 +0530

    Adding files

---
+++ mnist_cnn_torch.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import torch.nn.functional as F
